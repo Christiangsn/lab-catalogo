4fb88b8c63b03372c10f211913904891ccc00021
christiangsn <christianguimaraes1996@gmail.com>
2022-09-25 07:21:14 -0300

    feat: create a scope project

---
+++ src/infra/gateways/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  public async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  public async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+
+  public async enableShutDownHooks(app: INestApplication): Promise<void> {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
